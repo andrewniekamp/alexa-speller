4e7de82396ccde59a79eef6c8a86815fc1628cd1
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-10 15:46:01 -0500

    Commit all before transition to node-sdk

--- apps/speller/index.js
+++ apps/speller/index.js
@@ -17,7 +17,7 @@ const config = {
 const app = new Alexa.app('speller');
 const DictionaryDataHelper = require('./dictionary_data_helper');
 
-app.appId = process.env.ALEXA_APP_ID;
+app.id = process.env.ALEXA_APP_ID;
 
 app.launch((req, res) => {
   const prompt = 'Hello! You can ask me for a word, and specify by grade two through eight, if you like.';
@@ -63,15 +63,32 @@ app.intent('GetWordIntent', (req, res) => {
     return fbApp.delete();
   })
   .then( () => {
-    return res.say('You want your word again? Ok, it\'s ' + word + '.').shouldEndSession(false).send();
+    // let card = {
+    //   type: 'Standard',
+    //   title: 'Picture Test!',
+    //   text: 'Text area test, does this show?? \n This should be on a new line. Did it work?',
+    //   image: {
+    //     smallImageUrl: 'https://i.imgur.com/vqiRm.jpg',
+    //     largeImageUrl: 'https://i.imgur.com/vqiRm.jpg'
+    //   }
+    // }
+    var cardTitle = 'Hello World Card';
+    var cardContent = 'This text will be displayed in the companion app card.';
+
+    var imageObj = {
+        smallImageUrl: 'https://i.imgur.com/vqiRm.jpg',
+        largeImageUrl: 'https://i.imgur.com/vqiRm.jpg'
+    };
+    console.log('we got this far');
+    app.handler(event, context);
+    this.emit(':tellWithCard', 'lalalala', cardTitle, cardContent, imageObj);
+    // res.card('Some title', 'This is the content area');
+    // return res.say('You want your word again? Ok, it\'s ' + word + '.').shouldEndSession(false).send();
   })
   .catch( (err) => {
     console.log(err.statusCode);
     let prompt = 'Hmm, something went wrong. Let\'s try again.';
-    return fbApp.delete()
-    .then( () => {
-      res.say(prompt).reprompt(prompt).shouldEndSession(false).send();
-    })
+    res.say(prompt).reprompt(prompt).shouldEndSession(false).send();
   });
 })
 
@@ -79,7 +96,7 @@ app.intent('CheckSpellingIntent', (req, res) => {
   const fbApp = firebase.initializeApp(config);
   const userId = req.userId.split('.').join(''); // Must remove '.' from id
   let spelledWord;
-  if (req.slots.SpellingLetter.value) spelledWord = req.slots.SpellingLetter.value;
+  if (req.slots.SpellingLetter.value) spelledWord = req.slots.SpellingLetter.value.toLowerCase();
   let word;
   return firebase.database().ref('/users/' + userId).once('value')
   .then( data => {
